dc33da3d85a62894ee69d9a76de4096274e7f718
대현009 <hanmir009@naver.com>
2024-07-22 17:32:59 +0900

    20240722 수업내용 커밋

---
+++ 05_oop/src/method/StundentRun.java
@@ -0,0 +1,12 @@
+package method;
+
+public class StundentRun {
+	
+	public static void main(String[] args) {
+		
+		// StudentService 객체 생성
+		StudentService service = new StudentService();
+		
+		service.mainMenu();
+	}
+}
